6c8d31a9fe2db8192955a1f38ae8ea6f88b8bcc1
Juhani Riisiö <juhani.riisio@gmail.com>
2023-07-19 20:36:29 +0300

    add java project skeleton

---
+++ melodify/src/main/java/org/juhanir/Melodify.java
@@ -0,0 +1,13 @@
+package org.juhanir;
+
+/**
+ * Hello world!
+ *
+ */
+public class Melodify 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
