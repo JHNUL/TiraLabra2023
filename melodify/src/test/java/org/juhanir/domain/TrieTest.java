f424c1555efb9bc5651ea9c3c2d36964c2dae0d5
Juhani Riisiö <juhani.riisio@signanthealth.com>
2023-07-25 14:19:15 +0300

    implement prefix search for next note

--- melodify/src/test/java/org/juhanir/domain/TrieTest.java
+++ melodify/src/test/java/org/juhanir/domain/TrieTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class TrieTest {
@@ -12,6 +14,11 @@ class TrieTest {
     private final String[] oneChild = { "E#4quarter" };
     private final String[] twoChildren = { "E#4quarter", "F#4quarter" };
     private final String[] threeChildren = { "E#4quarter", "F#4quarter", "A#4quarter" };
+    private final String[] firstBranch = { "A", "B", "C" };
+    private final String[] secondBranch = { "A", "B", "D" };
+    private final String[] thirdBranch = { "A", "B", "E" };
+    private final String[] fourthBranch = { "A", "B", "F" };
+    private final String[] fifthBranch = { "A", "C", "F#" };
 
     @Test
     void canInstantiate() {
@@ -70,7 +77,82 @@ class TrieTest {
         assertNull(last);
         last = trie.lookup(oneChild);
         assertNull(last);
-        last = trie.lookup(new String[]{"FOOBAR"});
+        last = trie.lookup(new String[] { "FOOBAR" });
         assertNull(last);
+    }
+
+    @Test
+    void prefixSearchReturnsEmptyListWhenNotFound() {
+        Trie trie = new Trie();
+        trie.insert(threeChildren);
+        var result = trie.prefixSearch(new String[] { "yi", "er", "san", "si" });
+        assertInstanceOf(List.class, result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void prefixSearchReturnsEmptyListWhenPrefixEqualsKey() {
+        Trie trie = new Trie();
+        trie.insert(threeChildren);
+        var result = trie.prefixSearch(threeChildren);
+        assertInstanceOf(List.class, result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void prefixSearchReturnsEmptyListWhenPrefixEmpty() {
+        Trie trie = new Trie();
+        trie.insert(threeChildren);
+        var result = trie.prefixSearch(new String[] {});
+        assertInstanceOf(List.class, result);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    void prefixSearchReturnsImmediateChildrenWithOneNotePrefix() {
+        Trie trie = new Trie();
+        trie.insert(threeChildren);
+        var result = trie.prefixSearch(new String[] { threeChildren[0] });
+        assertEquals(1, result.size());
+        assertEquals(result.get(0).getValue(), threeChildren[1]);
+    }
+
+    @Test
+    void prefixSearchReturnsImmediateChildrenWithTwoNotePrefix() {
+        Trie trie = new Trie();
+        trie.insert(threeChildren);
+        var result = trie.prefixSearch(new String[] { threeChildren[0], threeChildren[1] });
+        assertEquals(1, result.size());
+        assertEquals(result.get(0).getValue(), threeChildren[2]);
+    }
+
+    @Test
+    void prefixSearchReturnsImmediateChildrenWhenExists() {
+        Trie trie = new Trie();
+        trie.insert(firstBranch);
+        trie.insert(secondBranch);
+        trie.insert(thirdBranch);
+        trie.insert(fourthBranch);
+        trie.insert(fifthBranch);
+        var result = trie.prefixSearch(new String[] { "A", "B" });
+        assertEquals(4, result.size());
+        assertEquals("C", result.get(0).getValue());
+        assertEquals("D", result.get(1).getValue());
+        assertEquals("E", result.get(2).getValue());
+        assertEquals("F", result.get(3).getValue());
+    }
+
+    @Test
+    void prefixSearchReturnsImmediateChildrenNoDuplicates() {
+        Trie trie = new Trie();
+        trie.insert(firstBranch);
+        trie.insert(secondBranch);
+        trie.insert(thirdBranch);
+        trie.insert(fourthBranch);
+        trie.insert(fifthBranch);
+        var result = trie.prefixSearch(new String[] { "A" });
+        assertEquals(2, result.size());
+        assertEquals("B", result.get(0).getValue());
+        assertEquals("C", result.get(1).getValue());
     }
 }
