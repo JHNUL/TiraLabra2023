c62572678ab2ac4a7b1202da9d2aa960d0a26269
Juhani Riisiö <43453258+JHNUL@users.noreply.github.com>
2023-07-27 22:38:41 +0300

    Populate trie from sublists of melody sequence (#12)

--- melodify/src/main/java/org/juhanir/services/TrainingService.java
+++ melodify/src/main/java/org/juhanir/services/TrainingService.java
@@ -1,9 +1,11 @@
 package org.juhanir.services;
 
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
+import org.juhanir.domain.Trie;
 import org.juhanir.utils.FileIO;
 import org.juhanir.utils.ScoreParser;
 
@@ -16,10 +18,12 @@ public class TrainingService {
 
     private final FileIO fileIo;
     private final ScoreParser scoreParser;
+    private final Trie trie;
 
-    public TrainingService(FileIO fileIo, ScoreParser scoreParser) {
+    public TrainingService(FileIO fileIo, ScoreParser scoreParser, Trie trie) {
         this.fileIo = fileIo;
         this.scoreParser = scoreParser;
+        this.trie = trie;
     }
 
     /**
@@ -35,8 +39,13 @@ public class TrainingService {
             try (InputStream is = this.fileIo.readFile(filePath)) {
                 List<Integer> melodies = this.scoreParser.parse(is);
                 trainingLogger.info(melodies.toString());
-                // TODO: populate Trie with the melody n-tuples based on degree of Markov Chain
-                // we're using
+                int degree = 2; // TODO: From user
+                for (int i = 0; i < melodies.size() - degree; i++) {
+                    int[] trainingTuple = melodies.subList(i, i + degree + 1).stream().mapToInt(Integer::intValue)
+                            .toArray();
+                    this.trie.insert(trainingTuple);
+                    trainingLogger.info(String.format("Inserted %s", Arrays.toString(trainingTuple)));
+                }
             } catch (Exception e) {
                 trainingLogger.severe("Failed to parse file " + filePath);
                 trainingLogger.severe(e.toString());
