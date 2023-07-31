69f1ac393cfe3e514fb6e73c08d5b766c2408634
Juhani Riisiö <juhani.riisio@gmail.com>
2023-07-31 18:33:29 +0300

    more tests

---
+++ docs/weeklies/week3.md
@@ -0,0 +1,10 @@
+## Week3
+
+Implemented more tests using actual training data and checking if the trie size is correct and all expected sequences are found.
+
+## Hours
+
+|Amount|What I did|
+|-|-|
+|3||
+|||
--- melodify/src/test/java/org/juhanir/services/TrainingServiceTest.java
+++ melodify/src/test/java/org/juhanir/services/TrainingServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.juhanir.domain.Trie;
 import org.juhanir.utils.FileIO;
@@ -16,7 +17,9 @@ import org.junit.jupiter.api.Test;
 public class TrainingServiceTest {
 
   private List<String> trainingDataPaths = new ArrayList<>();
-  private String wholeMelody = "DDAABBAGGFFEEEEDAAGFFFEAAGGFFEDDAABBAGGFFEED";
+  private int[] wholeMelody =
+      { 2, 2, 9, 9, 11, 11, 9, 7, 7, 6, 6, 4, 4, 4, 4, 2, 9, 9, 7, 6, 6, 6, 4,
+          9, 9, 7, 7, 6, 6, 4, 2, 2, 9, 9, 11, 11, 9, 7, 7, 6, 6, 4, 4, 2 };
 
   @BeforeEach
   void setUp() {
@@ -57,14 +60,47 @@ public class TrainingServiceTest {
     assertEquals(70, trie.size());
   }
 
-  // @Test
-  // void allFirstDegreeSequencesAreInTrie() {
-  //   Trie trie = new Trie();
-  //   TrainingService service =
-  //       new TrainingService(new FileIO(), new ScoreParser(), trie);
-  //   service.trainWith(this.trainingDataPaths, 1);
-  //   for (int i = 0; i < wholeMelody.length() - 1; i++) {
-      
-  //   }
-  // }
+  @Test
+  void allFirstDegreeSequencesAreInTrie() {
+    Trie trie = new Trie();
+    TrainingService service =
+        new TrainingService(new FileIO(), new ScoreParser(), trie);
+    service.trainWith(this.trainingDataPaths, 1);
+    for (int i = 0; i < wholeMelody.length - 1; i++) {
+      trie.lookup(Arrays.copyOfRange(wholeMelody, i, i + 2));
+    }
+  }
+
+  @Test
+  void allSecondDegreeSequencesAreInTrie() {
+    Trie trie = new Trie();
+    TrainingService service =
+        new TrainingService(new FileIO(), new ScoreParser(), trie);
+    service.trainWith(this.trainingDataPaths, 2);
+    for (int i = 0; i < wholeMelody.length - 2; i++) {
+      trie.lookup(Arrays.copyOfRange(wholeMelody, i, i + 3));
+    }
+  }
+
+  @Test
+  void allThirdDegreeSequencesAreInTrie() {
+    Trie trie = new Trie();
+    TrainingService service =
+        new TrainingService(new FileIO(), new ScoreParser(), trie);
+    service.trainWith(this.trainingDataPaths, 3);
+    for (int i = 0; i < wholeMelody.length - 3; i++) {
+      trie.lookup(Arrays.copyOfRange(wholeMelody, i, i + 4));
+    }
+  }
+
+  @Test
+  void allFourthDegreeSequencesAreInTrie() {
+    Trie trie = new Trie();
+    TrainingService service =
+        new TrainingService(new FileIO(), new ScoreParser(), trie);
+    service.trainWith(this.trainingDataPaths, 4);
+    for (int i = 0; i < wholeMelody.length - 4; i++) {
+      trie.lookup(Arrays.copyOfRange(wholeMelody, i, i + 5));
+    }
+  }
 }
