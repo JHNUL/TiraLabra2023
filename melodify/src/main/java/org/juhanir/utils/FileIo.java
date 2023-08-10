2bfc99d899a8679efe1cfa0eedae664be9cd8f3d
Juhani Riisiö <43453258+JHNUL@users.noreply.github.com>
2023-08-10 14:01:43 +0300

    Save musicxml (#27)
    * WIP: save generation as musicXML
    
    * add test with longer sequence
    
    * Add selection for generated files
    
    * add playback for generated musicxml files

--- melodify/src/main/java/org/juhanir/utils/FileIo.java
+++ melodify/src/main/java/org/juhanir/utils/FileIo.java
@@ -3,15 +3,20 @@ package org.juhanir.utils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.audiveris.proxymusic.ScorePartwise;
+import org.audiveris.proxymusic.util.Marshalling;
+import org.audiveris.proxymusic.util.Marshalling.MarshallingException;
 
 /**
  * Utility class for filesystem operations.
@@ -20,6 +25,11 @@ public class FileIo {
 
   public InputStream readFile(String filePath) throws FileNotFoundException {
     return new FileInputStream(new File(filePath));
+  }
+
+  public FileInputStream readFile(String folderPath, String fileName) throws FileNotFoundException {
+    String path = this.getFolderPath(folderPath);
+    return new FileInputStream(new File(path + File.separator + fileName));
   }
 
   /**
@@ -40,21 +50,65 @@ public class FileIo {
   }
 
   /**
+   * Collect all files with certain extension in the specified folder.
+   *
+   * @param folderPath path to the folder
+   * @param extension  file extension
+   * @return list of filepaths as string
+   */
+  public List<String> getAllFilePathsInFolder(String folderPath, String extension) {
+    // TODO: Change to work with built project as well
+    String path = this.getFolderPath(folderPath);
+    File dataFolder = new File(path);
+    if (dataFolder.exists() && dataFolder.isDirectory()) {
+      return Arrays.stream(dataFolder.listFiles())
+          .filter(file -> file.getName().endsWith(extension))
+          .map(File::getAbsolutePath)
+          .collect(Collectors.toList());
+    }
+    return new ArrayList<>();
+  }
+
+  /**
    * Write string content to a file.
    *
    * @param folderPath path to the folder
-   * @param fileName file name
-   * @param content content
+   * @param fileName   file name
+   * @param content    content
    * @throws IOException if write fails
    */
   public void writeToFile(String folderPath, String fileName, String content) throws IOException {
     String path = this.getFolderPath(folderPath);
     File dataFolder = new File(path);
     if (dataFolder.exists() && dataFolder.isDirectory()) {
-      PrintWriter pw =
-          new PrintWriter(new FileWriter(String.format("%s%s%s", path, File.separator, fileName)));
-      pw.print(content);
-      pw.close();
+      try (PrintWriter pw = new PrintWriter(
+          new FileWriter(String.format("%s%s%s", path, File.separator, fileName)))) {
+        pw.print(content);
+      }
+    } else {
+      throw new IOException("The specified folder does not exist.");
+    }
+  }
+
+  /**
+   * Write ScorePartwise content to a file.
+   *
+   * @param folderPath path to the folder
+   * @param fileName   file name
+   * @param score      content in ScorePartwise format
+   * @throws IOException          if write fails
+   * @throws MarshallingException if marshalling the scorepartwise to xml fails
+   */
+  public void writeToFile(String folderPath, String fileName, ScorePartwise score)
+      throws IOException, MarshallingException {
+    String path = this.getFolderPath(folderPath);
+    File dataFolder = new File(path);
+    if (dataFolder.exists() && dataFolder.isDirectory()) {
+      try (OutputStream os = new FileOutputStream(new File(dataFolder, fileName))) {
+        Marshalling.marshal(score, os, true, 2);
+      }
+    } else {
+      throw new IOException("The specified folder does not exist.");
     }
   }
 
--- melodify/src/main/java/org/juhanir/view/AppEventHandler.java
+++ melodify/src/main/java/org/juhanir/view/AppEventHandler.java
@@ -7,32 +7,57 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+
+import org.audiveris.proxymusic.ScorePartwise;
+import org.jfugue.integration.MusicXmlParser;
+import org.jfugue.pattern.Pattern;
+import org.jfugue.player.Player;
 import org.juhanir.domain.Trie;
 import org.juhanir.services.GeneratorService;
 import org.juhanir.services.TrainingService;
 import org.juhanir.utils.Constants;
 import org.juhanir.utils.FileIo;
 import org.juhanir.utils.ScoreParser;
+import org.staccato.StaccatoParserListener;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-
+/**
+ * Event handlers for UI elements.
+ */
 public class AppEventHandler {
 
   private final Trie trie;
   private final IntegerProperty degree;
   private final StringProperty musicalKey;
+  private final StringProperty playbackFile;
 
-  public AppEventHandler(Trie trie, IntegerProperty degree, StringProperty musicalKey) {
+  /**
+   * Constructor.
+   *
+   * @param trie         Trie data structure
+   * @param degree       Markov Chain degree
+   * @param musicalKey   Key the user selected
+   * @param playbackFile File the user selected for playback
+   */
+  public AppEventHandler(Trie trie, IntegerProperty degree, StringProperty musicalKey, StringProperty playbackFile) {
     this.trie = trie;
     this.degree = degree;
     this.musicalKey = musicalKey;
+    this.playbackFile = playbackFile;
   }
 
+  /**
+   * Event handler for degree textfield.
+   *
+   * @param degreeField UI element
+   */
   public void handleDegreeFieldChange(TextField degreeField) {
     degreeField.textProperty().addListener((observable, oldValue, newValue) -> {
       try {
@@ -46,6 +71,11 @@ public class AppEventHandler {
     });
   }
 
+  /**
+   * Event handler for musical key dropdown.
+   *
+   * @param musicalKeySelect UI element
+   */
   public void handleKeySelectChange(ComboBox<String> musicalKeySelect) {
     musicalKeySelect.valueProperty().addListener((observable, oldValue, newValue) -> {
       String key = newValue.split(" ")[0].strip();
@@ -53,6 +83,25 @@ public class AppEventHandler {
     });
   }
 
+  /**
+   * Event handler for playback file dropdown.
+   *
+   * @param playbackSelect UI element
+   */
+  public void handlePlaybackSelectChange(ComboBox<String> playbackSelect) {
+    playbackSelect.valueProperty().addListener((observable, oldValue, newValue) -> {
+      if (newValue != null) {
+        playbackFile.set(newValue);
+      }
+    });
+  }
+
+  /**
+   * Event handler for train button.
+   *
+   * @param trainButton UI element
+   * @param filesPerKey Training data files grouped per musical key
+   */
   public void handleTrainButtonClick(Button trainButton, Map<String, List<String>> filesPerKey) {
     trainButton.setOnAction(event -> {
       List<String> files = filesPerKey.getOrDefault(musicalKey.get(), Collections.emptyList());
@@ -74,8 +123,14 @@ public class AppEventHandler {
     });
   }
 
+  /**
+   * Event handler for generate button.
+   *
+   * @param generateButton UI element
+   * @param filesPerKey    Training data files grouped per musical key
+   */
   public void handleGenerateButtonClick(Button generateButton,
-      Map<String, List<String>> filesPerKey) {
+      Map<String, List<String>> filesPerKey, ObservableList<String> playbackFiles) {
     generateButton.setOnAction(event -> {
       if (degree.get() < 0) {
         return;
@@ -84,18 +139,62 @@ public class AppEventHandler {
       try {
         GeneratorService generator = new GeneratorService(trie, new Random());
         ScoreParser parser = new ScoreParser();
-        FileIo reader = new FileIo();
         int[] melody = generator.predictSequence(initialSequence, 50);
-        String[] notes = Arrays.stream(melody)
-            .mapToObj(note -> parser.convertIntToNote(note, musicalKey.get()).toString())
-            .toArray(String[]::new);
+        ScorePartwise score = parser.convertMelodyToScorePartwise(melody, musicalKey.get());
         LocalDateTime now = LocalDateTime.now();
         String fileName = musicalKey.get() + "_generation_"
-            + now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH.mm.ss"));
-        reader.writeToFile(Constants.OUTPUT_DATA_PATH, fileName, String.join(" ", notes));
+            + now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH.mm.ss.SSS"));
+        FileIo reader = new FileIo();
+        reader.writeToFile(Constants.OUTPUT_DATA_PATH, fileName + ".xml", score);
+        playbackFiles.add(fileName + ".xml");
+        reader.writeToFile(Constants.OUTPUT_DATA_PATH, fileName, Arrays.toString(melody));
       } catch (Exception e) {
         // TODO: report to the UI
         System.out.println(e.getMessage());
+      }
+    });
+  }
+
+  /**
+   * Event handler for playback button.
+   *
+   * @param playButton UI element
+   */
+  public void handlePlayButtonClick(Button playButton) {
+    playButton.setOnAction(event -> {
+      try {
+        Task<Void> playbackTask = new Task<Void>() {
+          @Override
+          protected Void call() throws Exception {
+            updateMessage("Playing generated file");
+            MusicXmlParser mxmlParser = new MusicXmlParser();
+            StaccatoParserListener listener = new StaccatoParserListener();
+            FileIo reader = new FileIo();
+            mxmlParser.addParserListener(listener);
+            mxmlParser.parse(reader.readFile(Constants.OUTPUT_DATA_PATH, playbackFile.get()));
+            Pattern staccatoPattern = listener.getPattern();
+            Player player = new Player();
+            player.play(staccatoPattern);
+            updateMessage("");
+            return null;
+          }
+        };
+
+        playbackTask.setOnSucceeded(taskEvent -> {
+          // TODO: some UI effect on success
+        });
+
+        playbackTask.messageProperty().addListener((observable, oldValue, newValue) -> {
+          // TODO: some UI effect on message
+        });
+
+        Thread thread = new Thread(playbackTask);
+        thread.setDaemon(true);
+        thread.start();
+
+      } catch (Exception e) {
+        // TODO: report to the UI
+        System.out.println(e);
       }
     });
   }
