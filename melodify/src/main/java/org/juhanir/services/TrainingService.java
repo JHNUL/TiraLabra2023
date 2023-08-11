30b52d4dae8196ec4ebb80e9ba9f44b9efbd6eb3
Juhani Riisiö <43453258+JHNUL@users.noreply.github.com>
2023-08-11 20:45:16 +0300

    Add new training data (#31)

--- melodify/src/main/java/org/juhanir/services/TrainingService.java
+++ melodify/src/main/java/org/juhanir/services/TrainingService.java
@@ -1,7 +1,6 @@
 package org.juhanir.services;
 
 import java.io.InputStream;
-// import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 import org.juhanir.domain.Trie;
@@ -39,24 +38,18 @@ public class TrainingService {
    * @param degree degree of Markov Chain to use
    */
   public void trainWith(List<String> filePaths, int degree) {
-
     for (final String filePath : filePaths) {
       try (InputStream is = this.fileIo.readFile(filePath)) {
         List<Integer> melodies = this.scoreParser.parse(is);
-        // trainingLogger.info(melodies.toString());
-        // trainingLogger.info(String.format("Len melodies: %s", melodies.size()));
         for (int i = 0; i < melodies.size() - degree; i++) {
           int[] trainingTuple =
               melodies.subList(i, i + degree + 1).stream().mapToInt(Integer::intValue).toArray();
-          // trainingLogger.info(String.format("Inserting: %s", Arrays.toString(trainingTuple)));
           this.trie.insert(trainingTuple);
         }
       } catch (Exception e) {
         trainingLogger.severe("Failed to parse file " + filePath);
         trainingLogger.severe(e.toString());
       }
-
     }
-
   }
 }
